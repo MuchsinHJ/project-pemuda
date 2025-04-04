2889e6039078c9ccccbab63bacd9bfa6119ee6ed
muchsinhj <muchsinhj10@gmail.com>
2025-04-04 14:02:58 +0700

    Memodifikasi section-home dan section-meaning dan menambahkan efek animasi

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,15 @@
 // Sticky navbar
-window.addEventListener("scroll", function() {
-  const header = document.querySelector('.container-navbar');
-  header.classList.toggle('sticky', window.scrollY > 100);
-})
+window.addEventListener("scroll", function () {
+  const header = document.querySelector(".container-navbar");
+  header.classList.toggle("sticky", window.scrollY > 100);
+  // efek section meaning
+  const meaning = document.querySelector(".svg-meaning");
+  meaning.classList.toggle("scrollEfek", window.scrollY > 200);
+  meaning.classList.toggle("efekBalik", window.scrollY < 210);
+  const teks = document.querySelector(".section-meaning-elemens");
+  teks.classList.toggle("scrollEfek", window.scrollY > 200);
+
+});
 
 // hide navbar
 const header = document.querySelector(".container-navbar");
@@ -11,23 +18,30 @@ let lastScrollY = 0;
 window.addEventListener("scroll", function () {
   showButtonUp();
   const currentScroll = this.scrollY;
-  header.classList.toggle('hide-nav', window.scrollY > lastScrollY);
+  header.classList.toggle("hide-nav", window.scrollY > lastScrollY);
   lastScrollY = currentScroll;
 });
 
-function showButtonUp(){
-  if(window.scrollY > 300){
-    buttonUp.classList.add('effect-buttonUp');
-  } else{
-    buttonUp.classList.remove('effect-buttonUp');
+function showButtonUp() {
+  if (window.scrollY > 300) {
+    buttonUp.classList.add("effect-buttonUp");
+  } else {
+    buttonUp.classList.remove("effect-buttonUp");
   }
 }
 
 // efek button up
-const buttonUp = document.querySelector('.button-up');
-buttonUp.addEventListener("click", function(){
+const buttonUp = document.querySelector(".button-up");
+buttonUp.addEventListener("click", function () {
   window.scrollTo({
     top: 0,
-    behavior: "smooth"
+    behavior: "smooth",
   });
-})
+});
+
+const buttonMeaning = document.querySelector(".button-meaning");
+buttonMeaning.addEventListener("click", () => {
+  document.getElementById("target-meaning").scrollIntoView({
+    behavior: "smooth",
+  });
+});
